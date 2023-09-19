f79e202241e8d2da1faeafafdfae34f14ed5bc19
Alex Gutjahr <alex@alexgutjahr.com>
2023-09-19 15:33:27 +0200

    Remove useless config

--- src/main/kotlin/dev/axgr/YouTubeConfig.kt
+++ src/main/kotlin/dev/axgr/YouTubeConfig.kt
@@ -4,11 +4,6 @@ import org.springframework.boot.context.properties.ConfigurationProperties
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager
-import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder
-import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository
-import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager
-import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository
 import org.springframework.web.client.RestClient
 
 @ConfigurationProperties("youtube")
@@ -24,17 +19,5 @@ class YouTubeConfig(private val props: YouTubeProperties) {
       .requestInterceptor(interceptor)
       .baseUrl(props.url)
       .build()
-  }
-
-  @Bean
-  fun manager(clientRegistrationRepository: ClientRegistrationRepository, authorizedClientRepository: OAuth2AuthorizedClientRepository): OAuth2AuthorizedClientManager {
-    val provider = OAuth2AuthorizedClientProviderBuilder.builder()
-      .clientCredentials()
-      .build()
-
-    val manager = DefaultOAuth2AuthorizedClientManager(clientRegistrationRepository, authorizedClientRepository)
-    manager.setAuthorizedClientProvider(provider)
-
-    return manager
   }
 }
